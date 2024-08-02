253080909b9754b2ee68497bd4d5c6edcb3a14be
Greg Pfeil <greg@technomadic.org>
2024-08-02 12:35:00 -0600

    Run reader tests without replacing reader
    We don’t want to require that the modified reader is loaded into Emacs for the
    tests to pass (don’t want to mess up someone’s session).

--- bradix-reader-tests.el
+++ bradix-reader-tests.el
@@ -5,14 +5,22 @@
 (require 'bradix-reader)
 (require 'buttercup)
 
+(defun bradix--read (string)
+  "Return the value read from the STRING."
+  (car (er-read-from-string string)))
+
 (describe "bradix-reader"
   (describe "reading"
+    (describe "sanity"
+      (it "is read as a number by elisp-reader"
+        (expect (type-of (bradix--read "⠼12345⠈")) :to-be 'integer)
+        (expect (type-of (bradix--read "⠼12345⠌6789AB")) :to-be 'float)))
     (it "handles arbitrary radices"
-      (expect ⠼12345⠌6789AB :to-equal 24677.553718640153)
-      (expect ⠼12345⠐6789ab :to-equal 74565.4044443965)
-      (expect ⠼12345⠈ :to-equal 5349)
-      (expect ⠼⠂01010101 :to-equal 0.33203125)
-      (expect ⠼-11100⠂01010101 :to-equal -28.33203125))))
+      (expect (bradix--read "⠼12345⠌6789AB") :to-equal 24677.553718640153)
+      (expect (bradix--read "⠼12345⠐6789ab") :to-equal 74565.4044443965)
+      (expect (bradix--read "⠼12345⠈") :to-equal 5349)
+      (expect (bradix--read "⠼⠂01010101") :to-equal 0.33203125)
+      (expect (bradix--read "⠼-11100⠂01010101") :to-equal -28.33203125))))
 
 (provide 'bradix-reader-tests)
 ;;; bradix-reader-tests.el ends here
